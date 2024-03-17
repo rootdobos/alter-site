0d5cc89b3dce2227bed48fa444244adbe0befe3e
rootdobos <49080519+rootdobos@users.noreply.github.com>
2024-03-17 23:34:50 +0200

    added some media query

--- src/components/event-list-item/event-list-item.component.jsx
+++ src/components/event-list-item/event-list-item.component.jsx
@@ -1,26 +1,22 @@
 import "./event-list-item.styles.scss";
 import { Link } from "react-router-dom";
 
-const EventListItem = ({ eventListItem,colorStyle }) => {
+const EventListItem = ({ eventListItem, colorStyle }) => {
   const { title, imageUrl, description } = eventListItem;
   return (
     // <div className="event-list-item">
-      <Link className={"event-list-item "+ colorStyle} to={eventListItem.slug}>
-        
-        <div className="event-list-item-details ">
-          <h3>{title}</h3>
-          <p>{description}</p>
+    <Link className={"event-list-item " + colorStyle} to={eventListItem.slug}>
+      <div className="event-list-item-details ">
+        <h3>{title}</h3>
+        <p>{description}</p>
 
         <div className="preview-images">
-        {
-          imageUrl.map((image)=>(
-          <img src={image} alt={title} /> 
-          ))
-        }
+          {imageUrl.map((image) => (
+            <img src={image} alt={title} />
+          ))}
         </div>
-        </div>
-        
-      </Link>
+      </div>
+    </Link>
     // </div>
   );
 };
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,25 +1,46 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import "./navigation.styles.scss";
+import { Menu, X } from "lucide-react";
+import { useMediaQuery, useMediaQueries } from "@react-hook/media-query";
 
-const Navigation =()=>{
-    return (
-        <Fragment>
-        <div className="navigation">
-          <Link className="logo-container" to="/">
-            <img className="main-logo" src={logo} alt="logo" />
-          </Link>
-  
-          <div className="navlinks">
-            <Link className="navlink" to="/">Főoldal</Link>
-            <Link className="navlink" to="/events">Rendezvényeink</Link>
-            <Link className="navlink" to="/gallery">Galéria</Link>
-            <Link className="navlink" to="/aboutus">Rólunk</Link>
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  const matches = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img className="main-logo" src={logo} alt="logo" />
+        </Link>
+        {!matches &&
+          <div onClick={toggleNavbar} className="navbar-button">
+            <button >{isOpen ? <X size={30} /> : <Menu size={30} />}</button>
           </div>
-        </div>
-        <Outlet />
-      </Fragment>
-    )
-}
-export default Navigation
+        }
+        {(matches || isOpen) && (
+          <div className={matches ? "navlinks-horizontal": "navlinks-vertical"}>
+            <Link className="navlink" to="/">
+              Főoldal
+            </Link>
+            <Link className="navlink" to="/events">
+              Rendezvényeink
+            </Link>
+            <Link className="navlink" to="/gallery">
+              Galéria
+            </Link>
+            <Link className="navlink" to="/aboutus">
+              Rólunk
+            </Link>
+          </div>
+        )}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
