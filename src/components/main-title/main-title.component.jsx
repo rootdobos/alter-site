6fd156f2ef22ca224b2c8901dfccc358c776ef21
rootdobos <49080519+rootdobos@users.noreply.github.com>
2024-03-17 13:56:22 +0200

    added video to main page

--- src/components/main-title/main-title.component.jsx
+++ src/components/main-title/main-title.component.jsx
@@ -1,13 +1,16 @@
-import "./main-title.styles.scss"
-const MainTitle=()=> {
-    return (
-<div className="main-title">
-      <h1 >
-        ALTERCREW
-      </h1>
+import "./main-title.styles.scss";
+import videoBg from "../../assets/intro.mp4";
+const MainTitle = () => {
+  return (
+    <div className="main-page-component">
+      <div className="main-page-overlay"></div>
 
+      <video src={videoBg} autoPlay loop muted />
+      <div className="main-page-headline">
+        <h2>ALTERCREW</h2>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default MainTitle
+export default MainTitle;
--- src/routes/gallery/gallery.component.jsx
+++ src/routes/gallery/gallery.component.jsx
@@ -1,6 +1,10 @@
 import mockGallery from "../../mock-gallery.json";
 
 const Gallery = () => {
+  const photos=mockGallery.map((item,i)=>(
+    {src:item.imageUrl, alt:`Image ${i + 1}`}
+  ))
+  console.log(photos)
   return (
     <div className="gallery-item">
       {mockGallery.map((item) => (
