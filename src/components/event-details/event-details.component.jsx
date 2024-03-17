61552dad065d0194fd95e864eabe82315ea27d58
rootdobos <49080519+rootdobos@users.noreply.github.com>
2024-03-17 02:11:32 +0200

    added details

--- src/components/event-details/event-details.component.jsx
+++ src/components/event-details/event-details.component.jsx
@@ -1,13 +1,28 @@
 import { useParams } from "react-router-dom";
 import mockEvents from "../../mock-events.json";
-const EventDetails =()=>{
-    const {slug}=useParams();
-    const eventData= mockEvents.filter((e)=> e.slug===slug)[0];
-return <div className="event-details-container">
-    <h3>{eventData.title}</h3>
-    <img src={eventData.imageUrl} alt="" />
-    <p>{eventData.description}</p>
-</div>
-
-}
-export default EventDetails
+import "./event-details.styles.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+const EventDetails = () => {
+  const { slug } = useParams();
+  const eventData = mockEvents.filter((e) => e.slug === slug)[0];
+  return (
+    <div className="event-details-container">
+      <h3>{eventData.title}</h3>
+      <p>{eventData.description}</p>
+      <Carousel
+        className="event-detail-carousel"
+        infiniteLoop={true}
+        autoPlay={true}
+        showThumbs={false}
+      >
+        {eventData.imageUrl.map((image, index) => (
+          <div>
+            <img src={image} alt={eventData.title + index} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+export default EventDetails;
