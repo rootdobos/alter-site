dde338b33df16e81a6dca5fba2bdaa6b4a14adbe
rootdobos <49080519+rootdobos@users.noreply.github.com>
2024-03-17 16:53:13 +0200

    added shadow to list elements, starting gallery and scroll to top

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import MainPage from "./routes/main-page/main-page.component";
 import Events from "./routes/events/events.components";
 import Gallery from "./routes/gallery/gallery.component";
 import AboutUs from "./routes/about-us/about-us.component";
+import ScrollToTop from "./utils/ui/scrollToTop";
 
-const App=()=> {
+const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigation/>}>
-        <Route index element={<MainPage />} />
-        <Route path="events/*" element={< Events/>} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="aboutus" element={<AboutUs />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<MainPage />} />
+          <Route path="events/*" element={<Events />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="aboutus" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
--- src/routes/gallery/gallery.component.jsx
+++ src/routes/gallery/gallery.component.jsx
@@ -1,15 +1,20 @@
 import mockGallery from "../../mock-gallery.json";
-
+import { ImageGallery } from "react-image-grid-gallery";
+import "./gallery.styles.scss"
 const Gallery = () => {
-  const photos=mockGallery.map((item,i)=>(
-    {src:item.imageUrl, alt:`Image ${i + 1}`}
-  ))
-  console.log(photos)
+  const photos = mockGallery.map((item, i) => ({
+    src: item.imageUrl,
+    alt: `Image ${i + 1}`,
+  }));
+  console.log(photos);
   return (
     <div className="gallery-item">
-      {mockGallery.map((item) => (
-        <img src={item.imageUrl} key={item.id} alt=""/>
-      ))}
+      <ImageGallery imagesInfoArray={photos} 
+      columnWidth={530} gapSize={36}
+      />
+      {/* {mockGallery.map((item) => (
+        <img src={item.imageUrl} key={item.id} alt="" />
+      ))} */}
     </div>
   );
 };
