c021f44815c039deeb4fe163d98c25890de44cf7
rootdobos <49080519+rootdobos@users.noreply.github.com>
2024-03-24 22:52:09 +0200

    fix empty dict bug

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,27 +1,25 @@
-export const slugify=(str)=> {
-    return String(str)
-      .normalize('NFKD') // split accented characters into their base characters and diacritical marks
-      .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-      .trim() // trim leading or trailing whitespace
-      .toLowerCase() // convert to lowercase
-      .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
-      .replace(/\s+/g, '') // replace spaces with hyphens
-      .replace(/-+/g, ''); // remove consecutive hyphens
-  }
+export const slugify = (str) => {
+  return String(str)
+    .normalize("NFKD") // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
+    .replace(/\s+/g, "") // replace spaces with hyphens
+    .replace(/-+/g, ""); // remove consecutive hyphens
+};
 
-export const getItemBySlug=(dict, slug)=>{
-  const slugParts=slug.split('-')
-  let result=dict
-  if(dict.length===0)
-    return []
-  slugParts.forEach((sp)=>{
-    result=result.filter((data)=>slugify(data.title)===sp)[0]
-    if(result.data){
-      result=result.data
-    }
-    else{
-      result=result.images
+export const getItemBySlug = (dict, slug) => {
+  const slugParts = slug.split("-");
+  let result = dict;
+  if (Object.keys(dict).length === 0) return [];
+  slugParts.forEach((sp) => {
+    result = result.filter((data) => slugify(data.title) === sp)[0];
+    if (result.data) {
+      result = result.data;
+    } else {
+      result = result.images;
     }
   });
-  return result
-}
+  return result;
+};
